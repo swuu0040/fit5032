ff7100a5a0afec31bedd113f45f2ea3304848a3c
Shiyu Wu <swuu0040@student.monash.edu>
2022-08-07 01:00:41 +0800

    week2 task2

---
+++ week2_task2/week2_task2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace week2_task2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ week2_task2/week2_task2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="week2_task2.MvcApplication" Language="C#" %>
